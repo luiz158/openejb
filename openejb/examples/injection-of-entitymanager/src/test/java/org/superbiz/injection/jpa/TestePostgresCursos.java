9edc84acb449f49c2c1b2d7dcf4f7623184b578e
Luiz Augusto <luiz158@gmail.com>
2012-06-22 14:56:16 -0300

    postgresql - curso

---
+++ openejb/examples/injection-of-entitymanager/src/test/java/org/superbiz/injection/jpa/TestePostgresCursos.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.superbiz.injection.jpa;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+
+/**
+ *
+ * @author leonardo
+ */
+public class TestePostgresCursos {
+
+  public static void main(String[] args) {
+    try {
+      final Properties p = new Properties();
+      p.put("movieDatabase", "new://Resource?type=DataSource");
+      p.put("movieDatabase.JdbcDriver", "org.postgresql.Driver");
+      p.put("movieDatabase.JdbcUrl", "jdbc:postgresql://avmbase:5432/curso2");
+
+      EJBContainer container = EJBContainer.createEJBContainer(p);
+      final Context context = container.getContext();
+
+      Movies movies = (Movies) context.lookup("java:global/injection-of-entitymanager/Movies");
+
+      movies.addMovie(new Movie("Quentin Tarantino", "Reservoir Dogs", 1992));
+      movies.addMovie(new Movie("Joel Coen", "Fargo", 1996));
+      movies.addMovie(new Movie("Joel Coen", "The Big Lebowski", 1998));
+
+      List<Movie> list = movies.getMovies();
+      //assertEquals("List.size()", 3, list.size());
+
+      for (Movie movie : list) {
+          System.out.format(" ---- %50s %50s %50s    ----\n", movie.getDirector(), movie.getTitle(), movie.getYear() );
+          movies.deleteMovie(movie);
+      }
+
+      //assertEquals("Movies.getMovies()", 0, movies.getMovies().size());
+
+      container.close();
+    } catch (Exception ex) {
+      Logger.getLogger(RodarEmbutido.class.getName()).log(Level.SEVERE, null, ex);
+//    } catch (NamingException ex) {
+//      Logger.getLogger(RodarEmbutido.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+}
